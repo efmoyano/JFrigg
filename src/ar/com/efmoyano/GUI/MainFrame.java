e6398db6018e89fc57b34929222191cd8dc5d265
Ernesto Moyano <ef.moyano@gmail.com>
2014-12-04 23:34:46 -0200

    Add tree test file model

--- src/ar/com/efmoyano/GUI/MainFrame.java
+++ src/ar/com/efmoyano/GUI/MainFrame.java
@@ -1,5 +1,10 @@
 package ar.com.efmoyano.GUI;
 
+import java.io.File;
+import javax.swing.JFrame;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
 public class MainFrame extends javax.swing.JFrame {
 
     public MainFrame() {
@@ -10,21 +15,49 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTree2 = new javax.swing.JTree();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jPanel1.setBackground(new java.awt.Color(255, 204, 204));
 
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
+        jTree2.setModel(new TestFileModel(new File("C:\\")));
+            jScrollPane1.setViewportView(jTree2);
+
+            javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+            jPanel1.setLayout(jPanel1Layout);
+            jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGap(41, 41, 41)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 539, Short.MAX_VALUE)
+                    .addGap(99, 99, 99))
+            );
+            jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addContainerGap(39, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(33, 33, 33))
+            );
+
+            getContentPane().add(jPanel1, java.awt.BorderLayout.LINE_END);
+
+            jMenu1.setText("File");
+            jMenuBar1.add(jMenu1);
+
+            jMenu2.setText("Edit");
+            jMenuBar1.add(jMenu2);
+
+            setJMenuBar(jMenuBar1);
+
+            pack();
+        }// </editor-fold>//GEN-END:initComponents
 
     public static void main(String args[]) {
         try {
@@ -44,5 +77,11 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTree jTree2;
     // End of variables declaration//GEN-END:variables
 }
